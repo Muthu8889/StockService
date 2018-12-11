5a300932bdcbb5ada305d768ebf2aa69db3a9414
Ubuntu <ubuntu@ip-172-31-36-97.ec2.internal>
2018-12-11 16:00:09 +0000

    Second commit

--- src/main/java/com/swl/stockwatchlist/StockwatchlistApplication.java
+++ src/main/java/com/swl/stockwatchlist/StockwatchlistApplication.java
@@ -2,11 +2,27 @@ package com.swl.stockwatchlist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericToStringSerializer;
 
 @SpringBootApplication
 public class StockwatchlistApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StockwatchlistApplication.class, args);
+	}
+	
+	@Bean
+	JedisConnectionFactory jedisConnectionFactory() {
+	    return new JedisConnectionFactory();
+	}
+	@Bean
+	public RedisTemplate<String, Object> redisTemplate() {
+	    final RedisTemplate<String, Object> template = new RedisTemplate<String, Object>();
+	    template.setConnectionFactory(jedisConnectionFactory());
+	    template.setValueSerializer(new GenericToStringSerializer<Object>(Object.class));
+	    return template;
 	}
 }
